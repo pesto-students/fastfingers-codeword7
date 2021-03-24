4e7201fd21e049a0a6da6ed518a601242a598d2c
Neeraj Yadav <ny29599@gmail.com>
2021-03-24 10:53:05 +0530

    game logic with design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-// import { reducer, initialState } from './reducer/sessionReducer';
 import GameComponent from './components/GameComponent/GameComponent';
 import StartComponent from './components/StartComponent/StartComponent';
-//export const UserContext = createContext();
 
 function createClickHandler(url) {
   return ev => {
-    // console.log('svbvbk', url)
     window.history.pushState({}, undefined, url);
     storePathInState(url.split("/").slice(1));
-    // ev.preventDefault();
   };
 }
 
@@ -22,30 +18,18 @@ const Home = ({ pathArr }) => {
   const [pathInState, setPathFn] = useState(pathArr);
   storePathInState = setPathFn;
 
-  return pathInState[0] === "start" ? (
-    <GameComponent createClickHandler={createClickHandler} />
-  ) : pathInState[0] === "about" ? (
-    'about'
+  return pathInState[0] === "start" && window.sessionStorage.getItem("name") ? (
+    <div className="App">
+      <GameComponent createClickHandler={createClickHandler} />
+    </div>
   ) : (
-    // Otherwise show the home page.
-    // <div>
-    //   There are two links: <br />
-    //   1.{" "}
-    //   <a href='/todos' onClick={createClickHandler("/todos")}>
-    //     Todos page
-    //   </a>
-    //   <br />
-    //   2.{" "}
-    //   <a href='/about' onClick={createClickHandler("/about")}>
-    //     About page
-    //   </a>
-    // </div>
-    <StartComponent createClickHandler={createClickHandler} />
+    <div className="App">
+      <StartComponent createClickHandler={createClickHandler} />
+    </div>
   );
 };
 
 function App() {
-  // const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <Home
       pathArr={window.location.pathname.split("/").slice(1)}
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { convertTimeToMilli, convertTimeToProgress } from "../../util";
+import CircularProgressBar from "../CircularProgressBar/CircularProgressBar";
+import "./Timer.css";
+
+export default function Timer({ time, handleGameOver }) {
+  const convertedTime = time * 100;
+  let [currentCount, setCount] = useState(convertedTime);
+  let timer = () => setCount(currentCount - 1);
+  const [initialStateChanged, setInitialStateChanged] = useState(false);
+
+  useEffect(() => {
+    if (convertedTime > 0) {
+      setInitialStateChanged(true);
+    }
+    setCount(convertedTime);
+  }, [convertedTime]);
+
+  useEffect(() => {
+    if (currentCount <= 0) {
+      if (initialStateChanged) {
+        handleGameOver();
+      }
+      return;
+    }
+    const intervalId = setInterval(timer, 6);
+    return () => clearInterval(intervalId);
+  });
+
+  return (
+    <div className="timer-container">
+      <CircularProgressBar
+        progress={convertTimeToProgress(currentCount, time)}
+      />
+      <div className="number">
+        <h2>{convertTimeToMilli(currentCount)}</h2>
+      </div>
+    </div>
+  );
+}
+
+Timer.propTypes = {
+  time: PropTypes.number.isRequired,
+  handleGameOver: PropTypes.func
+};
+Timer.defaultProps = {
+  time: 0,
+  handleGameOver: () => { }
+};
